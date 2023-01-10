16ffcd30b83b9f591c5ef475d28947f1f66895de
paulgr98 <pawelwnuk98@gmail.com>
2023-01-10 18:25:05 +0100

    Implemented adding memes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from components import ocr
 from components import toml_w
+from components import meme_manager
 import tomllib as tml
 import PySimpleGUI as sg
+import os
 
 
 def main() -> None:
@@ -9,12 +11,15 @@ def main() -> None:
     window = init_gui()
     while True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'exit':
-            break
-        elif event == 'find':
-            print('find')
-        elif event == 'add_folder':
-            on_add_folder_click()
+        match event:
+            case sg.WIN_CLOSED | 'exit':
+                break
+            case 'find':
+                print('find')
+            case 'add_folder':
+                on_add_folder_click()
+            case 'add_meme':
+                on_add_meme_click()
     window.close()
 
 
@@ -39,7 +44,8 @@ def init_gui() -> sg.Window:
             [
                 [sg.Text('')],  # spacer
                 [sg.Button('Find Meme!', key='find', size=(20, 1))],
-                [sg.Button('Add Meme Folder', key='add_folder', size=(20, 1))],
+                [sg.Button('Edit Meme Folder', key='add_folder', size=(20, 1))],
+                [sg.Button('Add Meme(s)', key='add_meme', size=(20, 1))],
                 [sg.Button('Exit', key='exit', size=(20, 1))]
             ],
             justification='center')],
@@ -50,11 +56,16 @@ def init_gui() -> sg.Window:
 
 def on_add_folder_click() -> None:
     add_folder_layout = [
-        [sg.Text('Your Meme Folder:')],
-        [sg.InputText(key='meme_folder', size=(30, 1)), sg.FolderBrowse('Browse', key='browse', size=(10, 1))],
-        [sg.Button('Save', key='save', size=(10, 1))]
+        [sg.Column(
+            [
+                [sg.Text('')],  # spacer
+                [sg.Text('Your Meme Folder:')],
+                [sg.InputText(key='meme_folder', size=(30, 1)), sg.FolderBrowse('Browse', key='browse', size=(10, 1))],
+                [sg.Button('Save', key='save', size=(10, 1))]
+            ],
+            justification='center')],
     ]
-    add_folder_window = sg.Window('Add Meme Folder', add_folder_layout, size=(400, 300))
+    add_folder_window = sg.Window('Edit Meme Folder', add_folder_layout, size=(400, 300))
 
     # read meme folder location from config
     config = load_config()
@@ -75,6 +86,56 @@ def on_add_folder_click() -> None:
                 add_folder_window.close()
                 break
     add_folder_window.close()
+
+
+def on_add_meme_click() -> None:
+    add_meme_layout = [
+        [sg.Column(
+            [
+                [sg.Text('')],  # spacer
+                [sg.Text('Update whole meme folder: '), sg.Button('Update', key='update', size=(10, 1))],
+                [sg.Text('OR')],
+                [sg.Text('Add single meme:')],
+                [sg.FileBrowse('Browse', key='browse', size=(10, 1), file_types=(("Image", "*.png *.jpg"),)),
+                 sg.Button('Add', key='add', size=(10, 1))],
+                [sg.Text('')],  # spacer
+                [sg.Text('', key='result', size=(30, 1))]
+            ],
+            justification='center')],
+    ]
+    add_meme_window = sg.Window('Add Meme(s)', add_meme_layout, size=(400, 300))
+    add_meme_window.finalize()
+
+    manager = meme_manager.MemeManager()
+
+    while True:
+        add_meme_event, add_meme_values = add_meme_window.read()
+        match add_meme_event:
+            case 'add':
+                meme_path = add_meme_values['browse']
+                meme_name = meme_path.split('/')[-1]
+                add_meme_window['result'].update(f'{meme_name} added')
+                break
+            case 'update':
+                config = load_config()
+                meme_folder = config['app']['meme_dir']
+                memes = load_memes(meme_folder)
+                manager.save_memes(memes)
+                add_meme_window['result'].update(f'{len(memes)} memes added')
+                break
+            case sg.WIN_CLOSED:
+                add_meme_window.close()
+                break
+
+
+def load_memes(path: str) -> list[str]:
+    # save full path to all .png and .jpg files in path
+    memes = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.png') or file.endswith('.jpg'):
+                memes.append(os.path.join(root, file))
+    return memes
 
 
 def save_config(config: dict) -> None:
---
+++ components/meme_manager.py
@@ -0,0 +1,38 @@
+import json
+import os
+from components import ocr
+
+
+class MemeManager(object):
+    def __init__(self):
+        self.memes_file_path = 'memes.json'
+
+    def load_memes(self) -> dict[str, list[str]]:
+        # check if memes.json exists
+        if not os.path.exists(self.memes_file_path):
+            with open(self.memes_file_path, 'w') as f:
+                json.dump({}, f)
+        # if file exists but doesn't contain json, delete it and create new one with empty dict
+        try:
+            with open(self.memes_file_path, 'r') as f:
+                _ = json.load(f)
+        except json.decoder.JSONDecodeError:
+            # delete the file
+            os.remove(self.memes_file_path)
+            # create new file with empty dict
+            with open(self.memes_file_path, 'w') as f:
+                json.dump({}, f)
+
+        with open(self.memes_file_path, 'r') as f:
+            memes = json.load(f)
+        return memes
+
+    def save_memes(self, memes: list[str]) -> None:
+        meme_content_dir = self.load_memes()
+        for meme in memes:
+            if meme not in meme_content_dir:
+                meme_img = ocr.get_image(meme)
+                meme_content_dir[meme] = ocr.get_text(meme_img)
+
+        with open(self.memes_file_path, 'w') as f:
+            json.dump(meme_content_dir, f, indent=4)
